527581451f7dcecc48298465a5c3406914a4b80e
Sascha Aeppli <munxar@gmx.de>
2015-05-22 18:15:55 +0200

    refactored databinding with properties.

--- app/notesApp.js
+++ app/notesApp.js
@@ -18,13 +18,66 @@
 
         // create properties
         this.filters = new Property();
+        this.filterIndex = new Property();
+        this.showFinished = new Property();
+        this.notes = new Property();
+        this.style = new Property();
+
+        // if filter index changes, recreate filter list
+        this.filterIndex.onChanged(this.setFilterIndex.bind(this));
     }
+
+    /**
+     * return available styles
+     * @returns {*[]}
+     */
+    NotesApp.prototype.getStyles = function() {
+        return [
+            { name: "Black White Style", href: "styles/default.css" },
+            { name: "Colorful Cat", href: "styles/color.css" }
+        ];
+    };
+
+    /**
+     * set the current active filter index
+     * @param index
+     */
+    NotesApp.prototype.setFilterIndex = function(index) {
+        // get property array
+        var filters = this.filters.get();
+        // update state
+        filters.forEach(function(filter, idx) { filter.active = idx == index; });
+        // reset property
+        this.filters.set(filters);
+    };
+
+    NotesApp.prototype.persist = function(key, property, init, noparse) {
+        var storage = this.storage;
+        var value = storage.getItem(key) || init;
+        if(!noparse) {
+            value = JSON.parse(value) || init;
+        }
+        property.set(value);
+        property.onChanged(function(value) {
+            storage.setItem(key, value);
+        });
+    };
 
     /**
      * initialize all properties to their defaults
      */
     NotesApp.prototype.init = function() {
-        this.filters.set([]);
+
+        this.filters.set([
+            { name: "finish date" },
+            { name: "creation date" },
+            { name: "importance" }
+        ]);
+
+        this.persist("filterIndex", this.filterIndex, 0);
+        this.persist("showFinished", this.showFinished, false);
+        this.persist("notes", this.notes, []);
+        this.persist("style", this.style, this.getStyles()[0], true)
     };
 
     // exports
--- index.js
+++ index.js
@@ -12,50 +12,72 @@
     var filter = document.getElementById("filter");
     var showFinished = document.getElementById("showFinished");
 
-    // initialize show finished toggle button
-    showFinished.className = app.getShowFinished() ? "active" : "";
-    // on click we toggle the button state and update the button
+    var notesApp = new exports.NotesApp(window.localStorage);
+
+    // listen to filters changes
+    notesApp.filters.onChanged(function(filters) {
+        while (filter.firstChild) {
+            filter.removeChild(filter.firstChild);
+        }
+        filters.forEach(function(f, index) {
+            var li = document.createElement("li");
+            li.className = f.active ? "active" : "";
+            var a = document.createElement("a");
+            a.innerHTML = f.name;
+            a.href = "";
+            a.onclick = function() {
+                notesApp.filterIndex.set(index);
+                event.preventDefault();
+            };
+            li.appendChild(a);
+            filter.appendChild(li);
+        });
+    });
+
+    // install change listener and set class depending on state
+    notesApp.showFinished.onChanged(function(show) {
+        showFinished.className = show ? "active" : "";
+    });
+
+    // on click we toggle showFinished flag
     showFinished.onclick = function() {
-        app.setShowFinished(!app.getShowFinished());
-        showFinished.className = app.getShowFinished() ? "active" : "";
+        notesApp.showFinished.set(!notesApp.showFinished.get());
+        // prevent default, so anchor doesn't navigate
+        event.preventDefault();
     };
-    
-    // create list of notes
-    app.getNotes().forEach(function(note, index) {
-        var li = document.createElement("li");
-        li.innerHTML = "<span>"+note.name+"</span><a href='edit.html?index=" + index + "'>bearbeiten</a>";
-        list.appendChild(li);
+
+    // render notes
+    notesApp.notes.onChanged(function(notes) {
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+        notes.forEach(function(note, index) {
+            var li = document.createElement("li");
+            li.innerHTML = "<span>"+note.name+"</span><a href='edit.html?index=" + index + "'>bearbeiten</a>";
+            list.appendChild(li);
+        });
     });
 
     // create styles list
-    app.getStyles().forEach(function(style, index) {
+    notesApp.getStyles().forEach(function(style) {
         var option = document.createElement("option");
         option.innerHTML = style.name;
         option.value = style.href;
         styles.appendChild(option);
     });
 
-    // set init and install change handler
-    styles.value = app.getStyle();
-    css.href = styles.value;
-    styles.onchange = function() {
-        app.setStyle(styles.value);
+    // bind style to select value
+    notesApp.style.onChanged(function(style) {
+        styles.value = style;
         css.href = styles.value;
-    };
-
-    // generate filters
-    app.getFilters().forEach(function(f, index) {
-        var li = document.createElement("li");
-        li.className = app.getFilter() == index ? "active" : "";
-        var a = document.createElement("a");
-        a.innerHTML = f.name;
-        a.href = "";
-        a.onclick = function() {
-            app.setFilter(index);
-        };
-        li.appendChild(a);
-        filter.appendChild(li);
     });
 
+    // on change set style
+    styles.onchange = function() {
+        notesApp.style.set(styles.value);
+    };
+
+    // init application
+    notesApp.init();
 
 })();
